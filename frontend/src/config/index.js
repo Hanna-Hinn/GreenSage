d26ad0e9b1446480b7fcdd0104368cbe5698a4f2
Hanna-Hinn <77826684+Hanna-Hinn@users.noreply.github.com>
2024-04-17 16:20:12 +0300

    Update index.js

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -4,7 +4,7 @@ export const server = dev ? "http://localhost:3000" : "";
 
 export const BACKEND_URL = dev
   ? "http://localhost:5000/api"
-  : "https://greensage-bcj5.onrender.com//api";
+  : "https://greensage-bcj5.onrender.com/api";
 
 export const SOCKET_URL = dev
   ? "http://localhost:5000"
